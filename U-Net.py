e1eea147f6eac5d1c50c605e5811ff8c946ad2ed
Vahid <vahid_negahdari@outlook.com>
2024-05-28 15:57:46 +0330

    2

--- U-Net.py
+++ U-Net.py
@@ -24,9 +24,9 @@ lr              = 0.003
 path  = Path('/home/cvl/Pycharm/pythonProject/Github')
 
 Density_test  = np.load(path / ('Density27.npy'), allow_pickle=True)
-Density_test  = np.reshape(Density_test, [BIGG_BATCH, n, n, 1])
+Density_test  = 1-np.reshape(Density_test, [BIGG_BATCH, n, n, 1])
 
-Appr_Rho_test = np.load(path / ('Appr_Rho27.npy'), allow_pickle=True)
+Appr_Rho_test = 1-np.load(path / ('Appr_Rho27.npy'), allow_pickle=True)
 Appr_Rho_test[np.abs(Appr_Rho_test) == inf] = 0
 Appr_Rho_test[np.abs(Appr_Rho_test) > 11] = np.mean(Appr_Rho_test)
 
@@ -130,10 +130,10 @@ for epoch in range(train_epochs):
     if np.mod(epoch, 2) == 0:
         lr = lr / 2
     for i in range(int(num_BIGG_BATCH)):
-         Density = np.load(path / ('Density' + str(i)+'.npy'), allow_pickle=True)
+         Density = 1-np.load(path / ('Density' + str(i)+'.npy'), allow_pickle=True)
          Density = np.reshape(Density,[BIGG_BATCH,n,n,1])
 
-         Appr_Rho = np.load(path / ('Appr_Rho' + str(i) + '.npy'), allow_pickle=True)
+         Appr_Rho = 1-np.load(path / ('Appr_Rho' + str(i) + '.npy'), allow_pickle=True)
          Appr_Rho[np.abs(Appr_Rho) == inf] = 0
          Appr_Rho[np.abs(Appr_Rho) > 11] = np.mean(Appr_Rho)
 
